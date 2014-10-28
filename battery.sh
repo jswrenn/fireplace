b6b8cb6a588dfa6b6a876a8f8087143b8003e746
John Wrenn <johnswrenn@gmail.com>
2014-10-28 19:41:20 -0400

    Restructured examples

--- battery.sh
+++
@@ -1,5 +0,0 @@
-FREQUENCY=0.1
-while true;
-do
-acpi -b | awk -F " |%" '{print $4}'
-done | ./target/fireplace 'Random Data'
--- test.sh
+++
@@ -1,8 +0,0 @@
-FREQUENCY=0.1
-T=0.0
-while true;
-do
-    echo "scale=5;s($T)" | bc -l;
-    T=$T+0.1;
-    sleep $FREQUENCY;
-done | ./target/fireplace -t 'Sin'
